a675f290dd4169dcae1806d4426f2b6e51056f79
martin-ngigi <martinwainaina001@gmail.com>
2024-05-06 07:18:59 +0300

    Initial commit

---
+++ SwiftCoreDataMVVM/SwiftCoreDataMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoreDataMVVMApp.swift
+//  SwiftCoreDataMVVM
+//
+//  Created by Martin Wainaina on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoreDataMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
